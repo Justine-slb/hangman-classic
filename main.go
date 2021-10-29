98f69ba25aac9ddb24ed1c1fd878a4e14318e1f2
JSEGUINLEBOULANGER <justine.seguinleboulanger@ynov.com>
2021-10-29 12:02:41 +0200

    4 - création fonction pour montrer de façon aléatoire n lettre du mot à trouver.

--- main.go
+++ main.go
@@ -32,44 +32,82 @@ func check(e error) {
 
 func main() {
 	rand.Seed(time.Now().UnixNano()) // fonction qui permet de générer un nouveau nombre random à chaque tour.
-	f, e := os.Open("C:\\Users\\justi\\OneDrive\\Documents\\YNOV\\YTRACK\\hangman-classic\\words2.txt")
+	f, e := os.Open("C:\\Users\\justi\\OneDrive\\Documents\\YNOV\\YTRACK\\hangman-classic\\dictionnaire.txt")
 	check(e)
 	defer f.Close()
-	data, _ := os.ReadFile("C:\\Users\\justi\\OneDrive\\Documents\\YNOV\\YTRACK\\hangman-classic\\words2.txt")
+	data, _ := os.ReadFile("C:\\Users\\justi\\OneDrive\\Documents\\YNOV\\YTRACK\\hangman-classic\\dictionnaire.txt")
 
-	tbWord := strings.SplitN(string(data), "\n", -1) // la fonction strings.SplitN permet de séparer le contenu du tableau à chaque saut de ligne. On crée ici un tableau de slice, chaque slice contient un mot.
-	ToFind := tbWord[rand.Intn(len(tbWord))]         // la fonction rand.Intn permet de générer un nbr random, grâce à la fonction rand.Seed le nbre random change à chaque tour.
+	tbWords := strings.SplitN(string(data), "\n", -1) // la fonction strings.SplitN permet de séparer le contenu du tableau à chaque saut de ligne. On crée ici un tableau de slice, chaque slice contient un mot.
+	toFind := tbWords[rand.Intn(len(tbWords))]        // la fonction rand.Intn permet de générer un nbr random, grâce à la fonction rand.Seed le nbre random change à chaque tour.
 	Attempt := 10
-	word := []rune(ToFind)
-	nbrL := len(ToFind)/2 + 1
-	var l string
 
-	for i := 0; i < len(ToFind); i++ {
-		if nbrL >= 0 {
-			j := rand.Intn(len(ToFind))
-			word[j] = '_'
-			nbrL--
-		}
+	nbrLShow := len(toFind)/2 - 1
+	var sugestL string
+	tbWord := []rune(toFind)
+	var retry bool
+	var tbSugestTry []string
+	for n := 0; n < len(tbWord); n++ {
+		tbWord[n] = '_'
 	}
 
-	fmt.Println(string(word))
+	fmt.Println(string(tbWord))
+	tbWord = CacheLettre(toFind, tbWord, nbrLShow)
 
 	for Attempt >= 0 {
 		fmt.Printf("you have %v attemps\n", Attempt)
+		fmt.Println(string(tbWord))
 		fmt.Printf("Propose a lower letter\n")
-		fmt.Scan(&l)
-		Error(l)
+		fmt.Scan(&sugestL)
+		Error(sugestL)
+		tbSugestTry = append(tbSugestTry, sugestL)
+		fmt.Println(tbSugestTry)
 
-		for i := 0; i < len(ToFind); i++ {
-			if l[0] == ToFind[i] {
-				word[i] = rune(ToFind[i])
-				fmt.Println(string(word))
-				if string(word) == ToFind {
+		for i := 0; i < len(toFind); i++ {
+			if sugestL[0] != toFind[i] {
+				retry = true
+			} else {
+				fmt.Println("Well done!")
+				tbWord[i] = rune(toFind[i])
+				fmt.Println(string(tbWord))
+				if string(tbWord) != toFind {
+					continue
+				} else {
 					fmt.Printf("You win\n")
 					return
 				}
 			}
 		}
+		if retry == true {
+			Attempt--
+		}
 	}
-	fmt.Println("You lost, the word to find was %v \n", ToFind)
+	fmt.Printf("You lost, the word to find was %v \n", toFind)
+}
+
+func CacheLettre(toFind string, tbWord []rune, nLshow int) []rune {
+	tb := HideLetters(nLshow, tbWord)
+	for i := 0; i < len(tb); i++ {
+		for j := 0; j < len(toFind); j++ {
+			if j == tb[i] {
+				tbWord[tb[i]] = rune(toFind[tb[i]])
+			}
+		}
+	}
+	return tbWord
+}
+
+func HideLetters(nLToShow int, tbWord []rune) []int {
+	rand.Seed(time.Now().UnixNano())
+	var tb []int
+
+	for i := 0; i < nLToShow; i++ {
+		Rnbr := rand.Intn(len(tbWord) - 1)
+		for j := 0; j < len(tb); j++ {
+			for Rnbr == tb[j] {
+				return HideLetters(nLToShow, tbWord)
+			}
+		}
+		tb = append(tb, Rnbr)
+	}
+	return tb
 }
