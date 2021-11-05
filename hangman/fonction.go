659d11152899b286942b9de7a11f40cb7d05faaa
JSEGUINLEBOULANGER <justine.seguinleboulanger@ynov.com>
2021-11-05 16:37:02 +0100

    11 mise à jour nom de variable + découpe des fonction hangmangame

--- hangman/fonction.go
+++ hangman/fonction.go
@@ -14,24 +14,26 @@ func IntroHang() {
 	fmt.Print("Hey you! What is your name ?\n")
 	name, _ := reader.ReadString('\n')
 	fmt.Print("Hello, "+name, "Time to play hangman!\n")
-	time.Sleep(2 * time.Second)
-	fmt.Println("Your mission, if you accept it, is too find the secret word.")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
+	fmt.Println("Your mission, if you accept it, is to find the secret word.")
+	time.Sleep(1 * time.Second)
 	fmt.Println("Let us give you some instructions: ")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("We give you a secret word to find.")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("You gonna propose letter or word")
-	time.Sleep(2 * time.Second)
-	fmt.Println("You gonna loose 1 attempt for each letter failed and 2 for each word failed")
-	fmt.Println("To help you, we gonna reveal some characters\nYou have 10 attempts to find it... ")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
+	fmt.Println("You gonna loose :\n-1 attempt for each letter failed\n-2 for each word failed")
+	fmt.Println("To help you, we gonna reveal some characters" +
+		"\nYou have 10 attempts to find it... ")
+	time.Sleep(1 * time.Second)
 	fmt.Println("If you fail ...")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 	PrintHangman(tbHangman, 10)
 	fmt.Println("José gonna be dead..")
 	fmt.Println("Start guessing...")
 	time.Sleep(1 * time.Second)
+
 	Restart()
 }
 
@@ -46,106 +48,67 @@ func Restart() {
 		fmt.Println("Oh okay... Hope to see you again, Bye...\n")
 		return
 	}
-
 }
 
 func gameHangman() {
 	toFind := ChooseWord()
 	tbHangman := ArrayHangman()
 	nbrLShow := len(toFind)/2 - 1
-
 	var lTried []string
-	word := PrintWord(toFind, nbrLShow)
+	wordHole := WordHoleF(toFind, nbrLShow)
 	attempt := 10
 	cptFailed := 0
-
 	for attempt > 0 {
-		failed := false
-		guestL := ProposeLetter(attempt, toFind, word, lTried)
-		lTried = append(lTried, guestL)
-		if len(guestL) == 1 {
-			for i := 0; i < len(toFind); i++ {
-				if guestL == string(toFind[i]) {
-					if word[i] == '_' {
-						word[i] = rune(toFind[i])
-						failed = true
-					}
-				}
-			}
-			if toFind == string(word) {
-				fmt.Printf("Congrats! YOU WON !! \n")
-				Restart()
-			}
+		inPut := CheckInPutF(attempt, toFind, wordHole, lTried)
+		lTried = append(lTried, inPut)
+		if len(inPut) == 1 {
+			failed := InPutLettre(inPut, toFind, wordHole)
 			if failed == false {
 				attempt--
 				cptFailed++
 				PrintHangman(tbHangman, cptFailed)
 			}
 		} else {
-			a := Compare(guestL, toFind)
-			fmt.Println(a)
-			if a == false {
-				fmt.Println("YOU WIN")
-				Restart()
-			} else {
+			if InPutWord(inPut, toFind) == false {
 				attempt -= 2
 				cptFailed += 2
 				PrintHangman(tbHangman, cptFailed)
 			}
 		}
 	}
-
 	if attempt == 0 {
 		fmt.Println("YOU LOOSE")
 		Restart()
 	}
 }
 
-func ProposeLetter(attempt int, toFind string, word []rune, lTried []string) string {
-	var sugestL string
+func CheckInPutF(attempt int, toFind string, word []rune, lTried []string) string {
+	inPut := InPutF(word, attempt)
+	err := IsAlpha(inPut)
+	if err == false {
+		return CheckInPutF(attempt, toFind, word, lTried)
+	} else if len(inPut) != len(toFind)-1 && len(inPut) != 1 {
+		a := len(toFind)
+		fmt.Printf("The number of argument is not exact. You can try a letter or a word with %v arguments.\n\n", a)
+		return CheckInPutF(attempt, toFind, word, lTried)
+	}
+	for _, lettre := range lTried {
+		if inPut == lettre {
+			fmt.Println("You already try this.\n")
+			return CheckInPutF(attempt, toFind, word, lTried)
+		}
+	}
+	return inPut
+}
+
+func InPutF(word []rune, attempt int) string {
+	var inPut string
 	fmt.Println(" ")
 	fmt.Println(string(word))
 	fmt.Printf("You have %v attemps\nChoose : ", attempt)
-	fmt.Scan(&sugestL)
-	sugestL = ToLower(sugestL)
-
-	if len(sugestL) == 1 {
-		err := IsAlpha(sugestL)
-		if err == false {
-			fmt.Println("You can only propose letter or word, special characters and numbers are not accepted\n")
-			ProposeLetter(attempt, toFind, word, lTried)
-		}
-	} else if len(sugestL) == len(toFind)-1 {
-		err := IsAlpha(sugestL)
-		for i := 0; i < len(sugestL); i++ {
-			if err == false {
-				ProposeLetter(attempt, toFind, word, lTried)
-			}
-		}
-	} else if len(sugestL)+1 != len(toFind) {
-		a := len(toFind)
-		fmt.Printf("The number of argument is not exact. You can try a letter or a word with %v arguments.\n\n", a)
-		ProposeLetter(attempt, toFind, word, lTried)
-	}
-
-	for _, lettre := range lTried {
-		if sugestL == lettre {
-			fmt.Println("You already try this.\n")
-			return ProposeLetter(attempt, toFind, word, lTried)
-		}
-	}
-	return sugestL
-}
-
-func ToLower(s string) string {
-	var newString string
-	for _, lettre := range []rune(s) {
-		if lettre >= 'A' && lettre <= 'Z' {
-			lettre += 32
-		}
-		newString += string(lettre)
-	}
-	return newString
+	fmt.Scan(&inPut)
+	inPut = strings.ToLower(inPut)
+	return inPut
 }
 
 func IsAlpha(s string) bool {
@@ -162,15 +125,46 @@ func PrintHangman(tbHangman []string, a int) { // fonction qui permet de génér
 	fmt.Println(tbHangman[a])
 }
 
-func Compare(a, b string) bool {
+func Compare(inPut, toFind string) bool {
 	different := false
-	for indexA := range a {
-		for i := 0; i < len(b); i++ {
-			if a[indexA] != b[indexA] {
+	for indexA := range inPut {
+		for i := 0; i < len(toFind); i++ {
+			if inPut[indexA] != toFind[indexA] {
 				different = true
 				return different
 			}
 		}
 	}
 	return different
+}
+
+func InPutLettre(inPut, toFind string, wordHole []rune) bool {
+	failed := false
+	for i := 0; i < len(toFind); i++ {
+		if inPut == string(toFind[i]) {
+			if wordHole[i] == '_' {
+				wordHole[i] = rune(toFind[i])
+				failed = true
+			}
+		}
+	}
+	if toFind == string(wordHole) {
+		fmt.Println(string(wordHole))
+		fmt.Printf("Congrats! YOU WON !! \n")
+		Restart()
+	}
+	if failed == true {
+		fmt.Println("Well done!")
+	}
+	return failed
+}
+
+func InPutWord(inPut, toFind string) bool {
+	a := Compare(inPut, toFind)
+	if a == false {
+		fmt.Println(toFind)
+		fmt.Println("YOU WIN")
+		Restart()
+	}
+	return false
 }
--- hangman/osfonction.go
+++ hangman/osfonction.go
@@ -34,29 +34,26 @@ func ArrayHangman() []string {
 	return tbHangman
 }
 
-func PrintWord(toFind string, nLshow int) []rune {
+func WordHoleF(toFind string, nLshow int) []rune {
 	tbWord := []rune(toFind)
 	for n := 0; n < len(tbWord)-1; n++ {
 		tbWord[n] = '_'
 	}
-	tbIndex := RandomIndex(nLshow, tbWord) // création d'un tableau d'index aléatoire gràce à la fonction HideLetters
-	for i := 0; i < len(tbIndex); i++ {    // on parcourt le tb index avec
-		for j := 0; j < len(toFind); j++ { // on parcourt le tb toFind
-			if j == tbIndex[i] { // quand la valeur comprise dans tbIndex[i] = index de toFind j'applique :
-				tbWord[tbIndex[i]] = rune(toFind[tbIndex[i]]) //
+	tbIndex := RandomIndex(nLshow, tbWord) // creation of a random index Array with the HideLetters function
+	for i := 0; i < len(tbIndex); i++ {    // loop for browse the tbIndex index by index
+		for j := 0; j < len(toFind); j++ { // loop to browse the toFind string
+			if j == tbIndex[i] { // when the value of tbIndex = value of j
+				tbWord[j] = rune(toFind[j]) // the function transforms "_" in the tbWord[j] by the value oh toFind[j]
 			}
-
 		}
 	}
 	return tbWord
 }
 
-func RandomIndex(nLToShow int, tbWord []rune) []int { // Cette fonction permet de créer un tableau d'index aléatoire, et tous différents
+func RandomIndex(nLToShow int, tbWord []rune) []int { // Array with random index all different
 	rand.Seed(time.Now().UnixNano())
 	var tbIndex []int
-	for n := 0; n < len(tbWord)-1; n++ { // on commence par modifier tous les caractères du tbWord par des _.
-		tbWord[n] = '_'
-	}
+
 	for i := 0; i < nLToShow; i++ {
 		Rnbr := rand.Intn(len(tbWord) - 1)
 		for j := 0; j < len(tbIndex); j++ {
